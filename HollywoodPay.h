70b424b13c379942199bf0f8654aa8f2b5d9a1ef
XuanTLe <56320361+XuanTLe@users.noreply.github.com>
2019-12-18 13:23:29 -0600

    Add files via upload

---
+++ HollywoodPay.h
@@ -0,0 +1,70 @@
+#pragma once
+#ifndef _EMPLOYEEINFO_
+#define _EMPLOYEEINFO_
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HollywoodPay
+{
+private:
+	struct HollywoodTree
+	{
+		float pay;
+		string name;
+		HollywoodTree* left; // left node
+		HollywoodTree* right; // right node
+		HollywoodTree(float p, string n)
+		{
+			pay = p;
+			name = n;
+			left = nullptr;
+			right = nullptr;
+		}
+	};
+
+	HollywoodTree* root; // root node of binary tree
+						 // private methods 
+	void insert(HollywoodTree* &, float, string);
+	void destroySubtree(HollywoodTree*);
+	void remove(HollywoodTree* &, string);
+	void makeDeletion(HollywoodTree* &);
+	void displayTree(HollywoodTree*);
+	// Public interface 
+public:
+	HollywoodPay()
+	{
+		root = NULL;
+	}
+
+	~HollywoodPay()
+	{
+		destroySubtree(root);
+	}
+	
+	HollywoodTree* getRootNode()
+	{
+		return root;
+	}
+
+
+	void insert(float p, string n)
+	{
+		insert(root, p, n);
+	}
+
+	bool search(HollywoodTree* node, string n);
+	
+	void remove(string n)
+	{
+		remove(root, n);
+	}
+
+	void showTree(void)
+	{
+		displayTree(root);
+	}
+};
+#include "HollywoodPay.cpp"
+#endif
